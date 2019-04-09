13dd25c9de4bf1125f4d9dd687fe674fd3ddafff
Ajiemar Santiago <ajiemar@gmail.com>
2019-04-09 14:55:36 -0500

    fix(BaseService): Fix misnamed properties

--- BaseService.cs
+++ BaseService.cs
@@ -42,8 +42,8 @@ namespace IBM.Cloud.SDK
 
                 string ApiKey = Environment.GetEnvironmentVariable(serviceId.ToUpper() + "_IAM_APIKEY");
                 // check for old IAM API key name as well
-                if (string.IsNullOrEmpty(apiKey)) {
-                    apiKey = Environment.GetEnvironmentVariable(ServiceName.ToUpper() + "_APIKEY");
+                if (string.IsNullOrEmpty(ApiKey)) {
+                    ApiKey = Environment.GetEnvironmentVariable(serviceId.ToUpper() + "_APIKEY");
                 }
                 string Username = Environment.GetEnvironmentVariable(serviceId.ToUpper() + "_USERNAME");
                 string Password = Environment.GetEnvironmentVariable(serviceId.ToUpper() + "_PASSWORD");
